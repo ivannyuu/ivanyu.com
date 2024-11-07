f44a285bac546c7bcf779e0e150a586e13461da7
ivannyuu <ivannyuu>
2024-11-06 23:18:52 -0800

    Move App.js from pages/home to src directory

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../../App.css';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <section id="projects">
+        <h2>Projects (TO DO)</h2>
+
+
+      </section>
+      <section id="experience">
+        <h2>Experience (TO DO)</h2>
+
+      </section>
+
+      <section id="education">
+        <h2>Education (TO DO)</h2>
+        
+      </section>
+      
+    </div>
+  );
+}
+
+export default App;
